513d9ec7f05692d013ba0a1ba2c04e0f282dedd3
Aseem Srivastava <aseemsrivastava9@gmail.com>
2022-12-23 16:35:00 +0530

    Added main file
    Created main screen for the game

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+screen = Screen()
+# Set Screen Color to black
+screen.bgcolor("black")
+# Set screen size to 800 x 600
+screen.setup(800, 600)
+# Set screen title to Pong
+screen.title("Pong")
+screen.exitonclick()
