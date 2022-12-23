706d810825809172822e7bb33c03fe5ca234bcb3
Aseem Srivastava <aseemsrivastava9@gmail.com>
2022-12-23 17:04:47 +0530

    Added Paddle
    Added paddle and movement control for paddle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import turtle
 from turtle import Screen
+from turtle import Turtle
 
 screen = Screen()
 # Set Screen Color to black
@@ -7,4 +9,38 @@ screen.bgcolor("black")
 screen.setup(800, 600)
 # Set screen title to Pong
 screen.title("Pong")
+screen.tracer(0)
+
+paddle = Turtle()
+# Set Position of Paddle
+paddle.setposition(350, 0)
+# Set Paddle color
+paddle.color("white")
+# Set Paddle shape
+paddle.shape("square")
+# Set paddle size
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+
+
+# Move paddle up
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+# Move paddle down
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 screen.exitonclick()
